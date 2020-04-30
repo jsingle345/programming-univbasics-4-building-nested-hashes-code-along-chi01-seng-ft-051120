b3c9923dc84824485516aa6088eb1b7497730f14
Joshua Singleton <jsingle345@gmail.com>
2020-04-30 20:35:54 +0000

    Automatically backed up by Learn

--- lib/04_fourth_challenge.rb
+++ lib/04_fourth_challenge.rb
@@ -10,11 +10,12 @@ def fourth_challenge
       ]
    }, 
    :capulet => {
-      :patriarch => {:name "Lord Capulet", :age "50"},
-      :matriarch => {:name "Lady Capulet", :age "51"},
-      :heroine => {:name "Juliet", :age "15", :status "alive"},
+      :patriarch => {:name => "Lord Capulet", :age => "50"},
+      :matriarch => {:name => "Lady Capulet", :age => "51"},
+      :heroine => {:name => "Juliet", :age => "15", :status => "alive"},
       :heroine_friends => [
-        :Steven => {:name => "Steven", a
+        :Steven => {:name => "Steven", :age => "30", :attitude => "confused"},
+        :Nurse => {:name => "Nurse", :age => "44", :attitude => "worried"}
       ]
    }
 }
